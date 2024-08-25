7329a14c65eda79c5b8c50b739d2c5f17ebbc80a
Sam Connelly <myclevorname@gmail.com>
2024-08-25 16:07:41 -0400

    0.1.0.1: Add documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,7 @@
 ## 0.1.0.0 -- 2024-08-25
 
 * First version. Released on an unsuspecting world.
+
+## 0.1.0.1 -- 2024-08-25
+
+- Added documentation.
--- src/Data/Random/TI.hs
+++ src/Data/Random/TI.hs
@@ -1,8 +1,24 @@
+{-|
+ -Module      : Data.Random.TI
+  Description : Implemenation of the TI-83+ RNG
+  Copyright   : (c) Samuel Connelly, 2024
+
+  License     : MIT
+  Maintainer  : myclevorname@gmail.com
+  Stability   : Stable
+  Portability : All
+
+  This module provides an implementation of the TI-83 Plus TI-BASIC random number
+  generator. The random number generators themselves produce a pair containing
+  the result and the random seed after the operation occurs.
+ - -}
+
 module Data.Random.TI (RandomSeed, rand, randInt, randIntList, initialSeed, makeSeed, randIntNoRep) where
 --import Data.Int (Int64)
 import Data.Ratio
 import Data.List (elemIndex)
 
+-- |The random seed as used by the random number generator. Typically, you'd want to use makeSeed.
 data RandomSeed = Seed Int Int
     deriving Show
 
@@ -13,10 +29,12 @@ modulus2 = 2147483399
 multiplier1 = 40014
 multiplier2 = 40692
 
+-- |The initial seed used by the random number generator.
 initialSeed :: RandomSeed
 
 initialSeed = Seed 12345 67890
 
+-- |Generates a random rational number between 0 and 1.
 rand :: RandomSeed -> (Ratio Int, RandomSeed)
 
 rand (Seed seed1 seed2) = (result, Seed newSeed1 newSeed2)
@@ -28,6 +46,7 @@ rand (Seed seed1 seed2) = (result, Seed newSeed1 newSeed2)
     result = if result1 < 0 then result1+1 else result1
 
 --         min    max
+-- |Generates a random integer between the first argument and second argument inclusively.
 randInt :: Int -> Int -> RandomSeed -> (Int, RandomSeed)
 
 randInt minValue maxValue origSeed = (result, newSeed)
@@ -37,6 +56,7 @@ randInt minValue maxValue origSeed = (result, newSeed)
     --                     Even if the random rational number happens to be >= 1, that isn't *too* bad.
     result = floor (x * (fromIntegral $ maxValue-minValue+1)) + minValue
 
+-- }Generates a list of random integers between the first argument and second argument inclusively.
 randIntList :: Int -> Int -> Int -> RandomSeed -> ([Int], RandomSeed)
 randIntList _ _ 0 seed = ([], seed)
 randIntList minValue maxValue count seed = (s:xs, finalSeed)
@@ -44,6 +64,7 @@ randIntList minValue maxValue count seed = (s:xs, finalSeed)
     (s, nextSeed) = randInt minValue maxValue seed
     (xs, finalSeed) = randIntList minValue maxValue (count-1) nextSeed
 
+-- |Generates a randok seed. Negative numbers map to their positive counterparts, and 0 returns initialSeed.
 makeSeed :: Int -> RandomSeed
 
 makeSeed n = case signum n of
@@ -52,6 +73,7 @@ makeSeed n = case signum n of
     1  -> Seed ((multiplier1 * n) `mod` modulus1) (n `mod` modulus2)
     _  -> undefined
 
+-- |Generates a list of random integers between the first argument and second argument inclusively. If the third argument is Nothing, then the function returns a random shuffle of [minValue..maxValue].
 randIntNoRep :: Int -> Int -> (Maybe Int) -> RandomSeed -> ([Int], RandomSeed)
 randIntNoRep _ _ (Just 0) seed = ([], seed)
 randIntNoRep minValue maxValue Nothing seed = loop [minValue..maxValue] [] seed
